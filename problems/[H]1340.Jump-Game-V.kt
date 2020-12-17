9fab0ee0107bf7cbcc151e6dcf0e3a412956afbb
Grigorii Raudiiainen <zub123456@gmail.com>
2020-12-17 20:08:45 +0300

    Update [H]1340.Jump-Game-V.kt

--- problems/[H]1340.Jump-Game-V.kt
+++ problems/[H]1340.Jump-Game-V.kt
@@ -1,41 +1,41 @@
 /**
- * Runtime: 808 ms, faster than 50.00% of Kotlin online submissions for Jump Game V.
- * Memory Usage: 71.8 MB, less than 50.00% of Kotlin online submissions for Jump Game V.
+ * Runtime: 388 ms, faster than 50.00% of Kotlin online submissions for Jump Game V.
+ * Memory Usage: 37.4 MB, less than 50.00% of Kotlin online submissions for Jump Game V.
  */
 
 class Solution {
     fun maxJumps(arr: IntArray, d: Int): Int {
-        var maxJumps = 0
-        val memo = mutableMapOf<Int, Int>()
+        var maxIndexesVisited = 0
+        val memo = IntArray(arr.size)
         for (i in 0 until arr.size) {
-            maxJumps = Math.max(maxJumps, getMaxJumpsCount(arr, i, d, memo))
+            maxIndexesVisited = Math.max(maxIndexesVisited, getMaxmaxIndexesVisited(arr, i, d, memo))
         }
-        return maxJumps + 1
+        return maxIndexesVisited
     }
     
-    fun getMaxJumpsCount(heights: IntArray, startPosition: Int, maxDistance: Int, memo: MutableMap<Int, Int>): Int {    
-        val savedJumps = memo.get(startPosition)
-        if (savedJumps != null) {
+    fun getMaxmaxIndexesVisited(heights: IntArray, startPosition: Int, maxDistance: Int, memo: IntArray): Int {    
+        val savedJumps = memo[startPosition]
+        if (savedJumps != 0) {
             return savedJumps
         }
         
         val currentHeight = heights[startPosition]
         val startIndex = Math.max(0, startPosition - maxDistance)
         val endIndex = Math.min(heights.size - 1, startPosition + maxDistance)
-        var maxJumps = 0
+        var maxIndexesVisited = 1
         for (i in startIndex .. endIndex) {
             if (i != startPosition) {
                 if (heights[i] < currentHeight) {
-                    maxJumps = Math.max(maxJumps, getMaxJumpsCount(heights, i, maxDistance, memo) + 1)
+                    maxIndexesVisited = Math.max(maxIndexesVisited, getMaxmaxIndexesVisited(heights, i, maxDistance, memo) + 1)
                 } else if (i < startPosition) {
-                    maxJumps = 0
+                    maxIndexesVisited = 1
                 } else {
                     break
                 }
             }
         }
         
-        memo.put(startPosition, maxJumps)
-        return maxJumps
+        memo[startPosition] = maxIndexesVisited
+        return maxIndexesVisited
     }
 }
