9de2a26cb4a0f06922d42c72af31ce186c3a1163
Grigorii Raudiiainen <zub123456@gmail.com>
2020-12-23 01:30:08 +0300

    Update [E]1464.Maximum-Product-of-Two-Elements-in-an-Array.kt

--- problems/[E]1464.Maximum-Product-of-Two-Elements-in-an-Array.kt
+++ problems/[E]1464.Maximum-Product-of-Two-Elements-in-an-Array.kt
@@ -1,17 +1,21 @@
 /**
- * Runtime: 164 ms, faster than 90.63% of Kotlin online submissions for Maximum Product of Two Elements in an Array.
- * Memory Usage: 37.1 MB, less than 15.63% of Kotlin online submissions for Maximum Product of Two Elements in an Array.
+ * Runtime: 156 ms, faster than 100.00% of Kotlin online submissions for Maximum Product of Two Elements in an Array.
+ * Memory Usage: 35.9 MB, less than 43.75% of Kotlin online submissions for Maximum Product of Two Elements in an Array.
  */
 
 class Solution {
+    
     fun maxProduct(nums: IntArray): Int {
         val maxNumbersQueue = PriorityQueue<Int>()
-        nums.forEach { number ->
-            maxNumbersQueue.offer(number)
-            if (maxNumbersQueue.size > 2) {
-                maxNumbersQueue.poll()
-            }
+        maxNumbersQueue.offer(nums[0])
+        maxNumbersQueue.offer(nums[1])
+        
+        for (index in 2 until nums.size) {
+            maxNumbersQueue.offer(nums[index])
+            maxNumbersQueue.poll()
         }
+        
         return (maxNumbersQueue.poll() - 1) * (maxNumbersQueue.poll() - 1)
     }
+    
 }
