996c21468b91da0781c6e3d632249f77d9154b1a
Grigorii Raudiiainen <zub123456@gmail.com>
2020-12-28 22:17:58 +0300

    Update [M]946.Validate-Stack-Sequences.kt

--- problems/[M]946.Validate-Stack-Sequences.kt
+++ problems/[M]946.Validate-Stack-Sequences.kt
@@ -1,10 +1,9 @@
 /**
- * Runtime: 200 ms, faster than 50.00% of Kotlin online submissions for Validate Stack Sequences.
- * Memory Usage: 38.7 MB, less than 16.67% of Kotlin online submissions for Validate Stack Sequences.
+ * Runtime: 188 ms, faster than 83.33% of Kotlin online submissions for Validate Stack Sequences.
+ * Memory Usage: 37.2 MB, less than 50.00% of Kotlin online submissions for Validate Stack Sequences.
  */
 class Solution {
     fun validateStackSequences(pushed: IntArray, popped: IntArray): Boolean {
-        val pushedValues = mutableSetOf<Int>()
         var pushList = LinkedList<Int>()
         val valuesCount = pushed.size
         var popIndex = 0
@@ -16,13 +15,8 @@ class Solution {
                 pushList.removeLast()
                 popIndex++
             }
-            if (popIndex < valuesCount && popped[popIndex] in pushedValues) {
-                return false
-            } else {
-                pushedValues.add(pushed[i])
-            }
         }
         
-        return true
+        return popIndex == valuesCount
     }
 }
