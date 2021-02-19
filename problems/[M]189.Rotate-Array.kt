3d6c04a0007430d505c3438b6db9bc87970db2ad
Grigorii Raudiiainen <zub123456@gmail.com>
2021-02-19 21:47:09 +0300

    Create [M]189.Rotate-Array.kt

---
+++ problems/[M]189.Rotate-Array.kt
@@ -0,0 +1,22 @@
+/**
+ * Runtime: 184 ms, faster than 85.37% of Kotlin online submissions for Rotate Array.
+ * Memory Usage: 36.5 MB, less than 61.95% of Kotlin online submissions for Rotate Array.
+ */
+class Solution {
+    fun rotate(nums: IntArray, k: Int): Unit {
+        val totalRotation = k % nums.size
+        val result = IntArray(nums.size)
+        var index = 0
+        for (i in totalRotation until nums.size) {
+            result[i] = nums[index]
+            index ++
+        }
+        for (i in 0 until totalRotation) {
+            result[i] = nums[index]
+            index ++
+        }
+        for (i in 0 until nums.size) {
+            nums[i] = result[i]
+        }
+    }
+}
